af44fb0fbc0c6077754f4913b35c2e916d287ac7
jlucasbx <jlucasb16@gmail.com>
2023-04-06 17:16:34 -0300

    adicionando animação no setas

--- src/js/registerForm.js
+++ src/js/registerForm.js
@@ -12,29 +12,47 @@ function selectFormSequence([start, end]) {
     });
 }
 
-function setActiveDotPosition(position){
-    dots.forEach((dot,index)=>{
+function setActiveDotPosition(position) {
+    dots.forEach((dot, index) => {
         let color = "#D9D9D9";
-        if(index === position){color = "#1864AB"}
+        if (index === position) { color = "#1864AB" }
         dot.style.backgroundColor = color;
     });
     setActiveRegisterButton(position);
+    setActiveControlButtons(position);
 }
 
 
-function setActiveRegisterButton(position){
+function setActiveRegisterButton(position) {
     let displayValue = "none";
-    if(position === (dots.length-1)){
+    if (position === (dots.length - 1)) {
         displayValue = "inline-block";
     }
     button.style.display = displayValue;
+}
+
+
+function setActiveControlButtons(position) {
+    let grayscaleNext = "rgba(74, 85, 104, 1)";
+    let grayscalePrev = "rgba(74, 85, 104, 1)";
+
+
+    if (position === 0) {
+        grayscalePrev = "rgba(74, 85, 104, 0.25)";
+    }
+    if (position === (dots.length - 1)) {
+        grayscaleNext = "rgba(74, 85, 104, 0.25)";
+    }
+
+    buttonPrev.style.borderColor = grayscalePrev;
+    buttonNext.style.borderColor = grayscaleNext;
 }
 
 function sendForm() {
     //Implementar depois
 }
 
-const buttonOnClick = (function(){
+const buttonOnClick = (function () {
     let position = 0;
     const sequences = [[0, 3], [4, 8], [9, 11]];
     selectFormSequence(sequences[position]);
