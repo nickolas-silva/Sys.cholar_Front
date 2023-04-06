191baef0568ff626b80b0df90930052f31e118a9
jlucasbx <jlucasb16@gmail.com>
2023-04-06 12:20:41 -0300

    adicionado suporte dinamico para multiplas paginas

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import fs from "fs";
+
+const files = fs.readdirSync(resolve(__dirname,"pages"));
+
+const objectFile = {};
+
+for (const file of files){
+    const fileName = file.replace(".html","");
+    objectFile[fileName] = resolve(file);
+}
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        register: resolve(__dirname, 'src/html/register.html'),
+        ...objectFile
       },
     },
   },
-})
+});
