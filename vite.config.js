675a3b90e6c49dce29593c8e787f038a57b757e7
jlucasbx <jlucasb16@gmail.com>
2023-04-08 10:47:15 -0300

    refatorando buscar páginas dinámico

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,34 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import fs from "fs";
 
-const files = fs.readdirSync(resolve(__dirname,"pages"));
-
-const objectFile = {};
-
-for (const file of files){
-    const fileName = file.replace(".html","");
-    objectFile[fileName] = resolve(file);
+function getFiles(path){
+  const files = fs.readdirSync(path);
+  return files.map(file=>resolve(__dirname,path,file));
 }
+
+function getFilesRecursive(path){
+  const originPath = resolve(__dirname,path);
+  const filesCache = new Array(1000);
+  const dirsCache = [originPath];
+  
+  while(dirsCache.length){
+    const files = getFiles(dirsCache.pop());
+    for(const file of files){
+      if(!file.includes(".")){
+        dirsCache.push(file);
+        continue;
+      }
+
+      filesCache.push(file);
+    }
+  }
+  
+  return filesCache;
+}
+
+const files = getFilesRecursive(resolve(__dirname,"pages"));
+const objectFile = {};
+files.forEach(file=>objectFile[file] = file);
 
 export default defineConfig({
   build: {
