c3df83db6db9a950ccdc9fd54163245798a64b7e
jlucasbx <jlucasb16@gmail.com>
2023-04-09 10:17:46 -0300

    telas diretor alunos finalizadas

--- src/js/disciplina.js
+++ src/js/disciplina.js
@@ -1,1 +1,6 @@
-console.log("ok")
+const listBody = document.querySelector(".list-body");
+
+
+listBody.addEventListener("click",(event)=>{
+    window.location.replace("/pages/diretor/disciplina-alunos.html");
+});
